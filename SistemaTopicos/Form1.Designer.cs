4626e59b448944dc2f20d5bfd82c905368361edd
victorcarreon <victorcarreone@gmail.com>
2014-11-20 17:42:12 -0700

    Primera Version lectura de codigo de barras, validacion, y guardar en db.

--- SistemaTopicos/Form1.Designer.cs
+++ SistemaTopicos/Form1.Designer.cs
@@ -29,11 +29,65 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.lblError = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.DarkGreen;
+            this.label1.Location = new System.Drawing.Point(86, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(213, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Escanear tablilla";
+            // 
+            // listView1
+            // 
+            this.listView1.Location = new System.Drawing.Point(12, 61);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(365, 134);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // lblError
+            // 
+            this.lblError.AutoSize = true;
+            this.lblError.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblError.ForeColor = System.Drawing.Color.Red;
+            this.lblError.Location = new System.Drawing.Point(12, 210);
+            this.lblError.Name = "lblError";
+            this.lblError.Size = new System.Drawing.Size(0, 18);
+            this.lblError.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(389, 261);
+            this.Controls.Add(this.lblError);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Registro de productos.";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Label lblError;
+
+
     }
 }
 
--- SistemaTopicos/Form1.cs
+++ SistemaTopicos/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO.Ports;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,85 @@ namespace SistemaTopicos
 {
     public partial class Form1 : Form
     {
+        SistemaTopicosEntities db;
+        List<TiposTablilla> tipos;
+        List<String> codigosValidos;
+       
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CheckForIllegalCrossThreadCalls = false;
+            db = new SistemaTopicosEntities();
+            //Obtener los tipos de tablilla validos
+            tipos = (from t in db.TiposTablilla
+                         select t).ToList();
+
+            //Los codigos validos para la validacion.
+            codigosValidos = (from t in tipos
+                                 select t.Id).ToList();
+            
+            serialPort1 = new SerialPort("COM1", 9600, Parity.None, 8, StopBits.One);
+            serialPort1.ReadTimeout = 1000;
+            serialPort1.WriteTimeout = 1000;
+            serialPort1.Encoding = Encoding.ASCII;
+            serialPort1.DataReceived += new SerialDataReceivedEventHandler(handler);
+            serialPort1.Open();
+
+            //Inicializa lista
+            listView1.View = View.Details;
+            listView1.GridLines = true;
+            listView1.FullRowSelect = true;
+
+            listView1.Columns.Add("Codigo", 70);
+            listView1.Columns.Add("Nombre", 100);
+            listView1.Columns.Add("Descripcion", 200);
+        }
+
+        private void handler(object sender, SerialDataReceivedEventArgs e)
+        {
+
+            string lectura = serialPort1.ReadLine();
+            lectura = lectura.Remove(lectura.Length - 1);
+            //Validar que la lectura del codigo es de una pieza esperada
+            if (codigosValidos.Contains(lectura))
+            {
+                var tempItem = (from d in tipos
+                                 where d.Id == lectura
+                                 select d).First();
+
+                string[] itemData = { tempItem.Id, tempItem.Nombre, tempItem.Descripcion };
+
+                ListViewItem item = new ListViewItem(itemData);
+                listView1.Items.Add(item);
+
+
+                //Guardar en inventario
+                Piezas nuevaPieza = new Piezas();
+                nuevaPieza.CodigoProveedor = tempItem.Id;
+                nuevaPieza.CodigoProceso = tempItem.Id + "I";
+                nuevaPieza.Descripcion = tempItem.Descripcion + " En Inventario";
+                nuevaPieza.timestamp = DateTime.Now;
+                nuevaPieza.EstadoFk = 1;
+                nuevaPieza.TipoFk = tempItem.Id;
+                nuevaPieza.Nombre = tempItem.Nombre + " En inventario";
+
+                db.Piezas.Add(nuevaPieza);
+                db.SaveChanges();
+
+            }
+            else
+            {
+                lblError.Text = "Elemento desconocido!, Escanear objeto valido.";
+               
+            }
+               
+                
+                
+        }
     }
+
 }
